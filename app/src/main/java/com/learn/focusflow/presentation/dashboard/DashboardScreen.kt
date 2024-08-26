1d788ca879d4d2db71f4e825d3e38e7517784e88
DeeKay02 <48171972+DeeKay02@users.noreply.github.com>
2024-08-26 16:49:43 +0530

    Add Subject Cards

--- app/src/main/java/com/learn/focusflow/presentation/dashboard/DashboardScreen.kt
+++ app/src/main/java/com/learn/focusflow/presentation/dashboard/DashboardScreen.kt
@@ -3,6 +3,7 @@ package com.learn.focusflow.presentation.dashboard
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
@@ -11,6 +12,8 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.LazyRow
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
 import androidx.compose.material3.CenterAlignedTopAppBar
@@ -30,9 +33,19 @@ import androidx.compose.ui.unit.dp
 import com.learn.focusflow.R
 import com.learn.focusflow.domain.model.Subject
 import com.learn.focusflow.presentation.components.CountCard
+import com.learn.focusflow.presentation.components.SubjectCard
 
 @Composable
 fun DashboardScreen() {
+
+    val subjects = listOf(
+        Subject(name = "Learn", goalHours = 10f, colors = Subject.subjectCardColors[0]),
+        Subject(name = "Work", goalHours = 10f, colors = Subject.subjectCardColors[1]),
+        Subject(name = "Exercise", goalHours = 10f, colors = Subject.subjectCardColors[2]),
+        Subject(name = "Break", goalHours = 10f, colors = Subject.subjectCardColors[3]),
+        Subject(name = "Sleep", goalHours = 10f, colors = Subject.subjectCardColors[4])
+    )
+
     Scaffold(
         topBar = { DashboardScreenTopBar() }
     ) { paddingValues ->
@@ -54,7 +67,7 @@ fun DashboardScreen() {
             item {
                 SubjectCardsSection(
                     modifier = Modifier.fillMaxWidth(),
-                    subjectList = emptyList()
+                    subjectList = subjects
                 )
             }
         }
@@ -68,7 +81,7 @@ private fun DashboardScreenTopBar() {
         title = {
             Text(
                 text = "FocusFlow",
-                style = MaterialTheme.typography.headlineMedium
+                style = MaterialTheme.typography.headlineLarge
             )
         }
     )
@@ -141,6 +154,18 @@ private fun SubjectCardsSection(
                color = Color.Gray,
                textAlign = TextAlign.Center
            )
+        }
+        LazyRow(
+            horizontalArrangement = Arrangement.spacedBy(12.dp),
+            contentPadding = PaddingValues(start = 12.dp, end = 12.dp)
+        ) {
+            items(subjectList) {subject ->
+                SubjectCard(
+                    subjectName = subject.name,
+                    gradientColors = subject.colors,
+                    onClick = {}
+                )
+            }
         }
     }
 }
