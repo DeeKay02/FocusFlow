9e4842340ac3350bc9b447fb3f724fd7f9063ad5
DeeKay02 <48171972+DeeKay02@users.noreply.github.com>
2024-08-26 15:48:29 +0530

    Add dashboard screen and count cards

--- app/src/main/java/com/learn/focusflow/MainActivity.kt
+++ app/src/main/java/com/learn/focusflow/MainActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import com.learn.focusflow.presentation.dashboard.DashboardScreen
 import com.learn.focusflow.presentation.theme.FocusFlowTheme
 
 class MainActivity : ComponentActivity() {
@@ -12,7 +13,7 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             FocusFlowTheme {
-
+                DashboardScreen()
             }
         }
     }
--- app/src/main/java/com/learn/focusflow/presentation/dashboard/DashboardScreen.kt
+++ app/src/main/java/com/learn/focusflow/presentation/dashboard/DashboardScreen.kt
@@ -1,2 +1,81 @@
 package com.learn.focusflow.presentation.dashboard
 
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material3.CenterAlignedTopAppBar
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import com.learn.focusflow.presentation.components.CountCard
+
+@Composable
+fun DashboardScreen() {
+    Scaffold(
+        topBar = { DashboardScreenTopBar() }
+    ) { paddingValues ->
+        LazyColumn(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(paddingValues)
+        ) {
+            item {
+                CountCardsSection(
+                    modifier = Modifier.fillMaxWidth().padding(12.dp),
+                    topicCount = 5,
+                    focusedHours = "10",
+                    goalHours = "15"
+                )
+            }
+        }
+    }
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+private fun DashboardScreenTopBar() {
+    CenterAlignedTopAppBar(
+        title = {
+            Text(
+                text = "FocusFlow",
+                style = MaterialTheme.typography.headlineMedium
+            )
+        }
+    )
+}
+
+@Composable
+private fun CountCardsSection(
+    modifier: Modifier,
+    topicCount: Int,
+    focusedHours: String,
+    goalHours: String
+) {
+    Row(modifier = modifier) {
+        CountCard(
+            modifier = Modifier.weight(1f),
+            headingText = "Topic Counts",
+            count = "$topicCount"
+        )
+        Spacer(modifier = Modifier.width(10.dp))
+        CountCard(
+            modifier = Modifier.weight(1f),
+            headingText = "Focused Hours",
+            count = focusedHours
+        )
+        Spacer(modifier = Modifier.width(10.dp))
+        CountCard(
+            modifier = Modifier.weight(1f),
+            headingText = "Goal Focus Hours",
+            count = goalHours
+        )
+    }
+}
