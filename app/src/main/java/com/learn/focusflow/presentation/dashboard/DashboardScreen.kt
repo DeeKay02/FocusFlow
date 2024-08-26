599519f7e57aa1eacb1dab72b127bb1ec53a4d7c
DeeKay02 <48171972+DeeKay02@users.noreply.github.com>
2024-08-26 16:14:32 +0530

    Add SubjectCardsSection

--- app/src/main/java/com/learn/focusflow/presentation/dashboard/DashboardScreen.kt
+++ app/src/main/java/com/learn/focusflow/presentation/dashboard/DashboardScreen.kt
@@ -1,20 +1,34 @@
 package com.learn.focusflow.presentation.dashboard
 
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
 import androidx.compose.material3.CenterAlignedTopAppBar
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
+import com.learn.focusflow.R
+import com.learn.focusflow.domain.model.Subject
 import com.learn.focusflow.presentation.components.CountCard
 
 @Composable
@@ -29,10 +43,18 @@ fun DashboardScreen() {
         ) {
             item {
                 CountCardsSection(
-                    modifier = Modifier.fillMaxWidth().padding(12.dp),
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(12.dp),
                     topicCount = 5,
                     focusedHours = "10",
                     goalHours = "15"
+                )
+            }
+            item {
+                SubjectCardsSection(
+                    modifier = Modifier.fillMaxWidth(),
+                    subjectList = emptyList()
                 )
             }
         }
@@ -77,5 +99,48 @@ private fun CountCardsSection(
             headingText = "Goal Focus Hours",
             count = goalHours
         )
+    }
+}
+
+@Composable
+private fun SubjectCardsSection(
+    modifier: Modifier,
+    subjectList: List<Subject>,
+    emptyListText: String = "You don't have any topics yet.\n Click the + button to add new topic."
+) {
+    Column {
+        Row(
+            modifier = Modifier.fillMaxWidth(),
+            horizontalArrangement = Arrangement.SpaceBetween,
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            Text(
+                text = "TOPICS",
+                style = MaterialTheme.typography.bodySmall,
+                modifier = Modifier.padding(start = 12.dp)
+            )
+            IconButton(onClick = { /*TODO*/ }) {
+                Icon(
+                    imageVector = Icons.Default.Add,
+                    contentDescription = "Add Subject"
+                )
+            }
+        }
+        if (subjectList.isEmpty()) {
+           Image(
+               modifier = Modifier
+                   .size(120.dp)
+                   .align(Alignment.CenterHorizontally),
+               painter = painterResource(R.drawable.img_books),
+               contentDescription = emptyListText
+           )
+           Text(
+               modifier = Modifier.fillMaxWidth(),
+               text = emptyListText,
+               style = MaterialTheme.typography.bodySmall,
+               color = Color.Gray,
+               textAlign = TextAlign.Center
+           )
+        }
     }
 }
